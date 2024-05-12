b6745a4af6b54a37bf73d38b94bb46ec2c8447ff
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2024-05-13 01:06:29 +0900

    Implement REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Write};
+
+use pure_lisp::{environment::Environment, lexer::Lexer, parser::Parser, syntax::Value};
+
+fn main() -> io::Result<()> {
+    let mut env = Environment::new();
+
+    loop {
+        print!("> ");
+        io::stdout().flush()?;
+
+        let mut input = String::new();
+        if io::stdin().read_line(&mut input)? == 0 {
+            // NOTE: Finish the program when the user presses Ctrl-D
+            return Ok(());
+        }
+
+        match Parser::new(Lexer::new(&input.to_string())).parse() {
+            Some(term) => match term.eval(&mut env) {
+                Some(value) => println!("{}", to_string_value(&value)),
+                None => println!("Semantics error"),
+            },
+            None => println!("Syntax error"),
+        }
+    }
+}
+
+fn to_string_value(value: &Value) -> String {
+    match value {
+        Value::Symbol(s) => s.clone(),
+        Value::Pair(car, cdr) => format!("({} {})", to_string_value(car), to_string_value(cdr)),
+        Value::Closure(_, _, _) => "<closure>".to_string(),
+    }
 }
